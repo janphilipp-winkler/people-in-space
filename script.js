faf64fe4bff5420cb028dd62596d8b9cb0bc528c
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2023-12-14 22:05:35 +0100

    names change now according to api data

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const text = document.querySelector('[data-js="text"]');
 const firstSentence = document.querySelector('[data-js="firstSentence"]');
+const secondSentence = document.querySelector('[data-js="secondSentence"]');
 const numberOfAstronauts = document.querySelector('[data-js="number"]');
 const namesOfAstronauts = document.querySelector('[data-js="names"]');
 
@@ -19,17 +20,27 @@ async function getNames() {
     document.querySelector("#location").addEventListener("change", function () {
       if (this.value == "space") {
         console.log("SPACE selected");
-        firstSentence.innerHTML = `in`;
+        firstSentence.innerHTML = "in";
+        secondSentence.innerHTML = "Their names are: ";
         numberOfAstronauts.innerHTML = data.number;
+        namesOfAstronauts.innerHTML = "";
+        data.people.forEach((name, index) => {
+          namesOfAstronauts.append((index ? ", " : "") + name.name);
+        });
       }
       if (this.value == "iss") {
         console.log("ISS selected");
         const isISS = data.people.filter((craft) => {
           return craft.craft.startsWith("ISS");
         });
-
-        firstSentence.innerHTML = `on the`;
+        console.log(isISS);
+        firstSentence.innerHTML = "on the";
+        secondSentence.innerHTML = "Their names are: ";
         numberOfAstronauts.innerHTML = isISS.length;
+        namesOfAstronauts.innerHTML = "";
+        isISS.forEach((name, index) => {
+          namesOfAstronauts.append((index ? ", " : "") + name.name);
+        });
       }
       if (this.value == "tiangong") {
         console.log("Tiangong selected");
@@ -38,6 +49,14 @@ async function getNames() {
         });
         firstSentence.innerHTML = `on the`;
         numberOfAstronauts.innerHTML = isTiangong.length;
+        namesOfAstronauts.innerHTML = "";
+        isTiangong.forEach((name, index) => {
+          namesOfAstronauts.append((index ? ", " : "") + name.name);
+        });
+        console.log(isTiangong.length);
+        if (isTiangong.length === 0) {
+          secondSentence.innerHTML = "";
+        }
       }
     });
   } catch (error) {
