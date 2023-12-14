569e31a8a4799c05411ab43d8bbde1b854bfc935
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2023-12-14 21:04:59 +0100

    implemented dropdown for filtering

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const text = document.querySelector('[data-js="text"]');
+const firstSentence = document.querySelector('[data-js="firstSentence"]');
 const numberOfAstronauts = document.querySelector('[data-js="number"]');
 const namesOfAstronauts = document.querySelector('[data-js="names"]');
 
@@ -14,6 +15,26 @@ async function getNames() {
     numberOfAstronauts.innerHTML = data.number;
     data.people.forEach((name, index) => {
       namesOfAstronauts.append((index ? ", " : "") + name.name);
+    });
+    document.querySelector("#location").addEventListener("change", function () {
+      if (this.value == "space") {
+        console.log("SPACE selected");
+        firstSentence.innerHTML = `There are currently <span data-js="number">${data.number}</span> people in`;
+      }
+      if (this.value == "iss") {
+        console.log("ISS selected");
+        const isISS = data.people.filter((craft) => {
+          return craft.craft.startsWith("ISS");
+        });
+        firstSentence.innerHTML = `There are currently <span data-js="number">${isISS.length}</span> people on the`;
+      }
+      if (this.value == "tiangong") {
+        console.log("Tiangong selected");
+        const isTiangong = data.people.filter((craft) => {
+          return craft.craft.startsWith("Tiangong");
+        });
+        firstSentence.innerHTML = `There are currently <span data-js="number">${isTiangong.length}</span> people on the`;
+      }
     });
   } catch (error) {
     console.error("Error fetching:", error.message);
